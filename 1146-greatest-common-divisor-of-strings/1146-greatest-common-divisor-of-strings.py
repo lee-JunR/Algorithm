7fc09b63f818b9b9b603647ec9893069c08aa1d2
lee-JunR <68640939+lee-JunR@users.noreply.github.com>
2023-09-21 16:09:12 +0900

    Time: 38 ms (70.42%), Space: 16.2 MB (93.39%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -1,11 +1,23 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        '''
-        if str2.find(str1[str1.find(str2):len(str2)]) == '':
-            return str2(str1[str1.find(str2):len(str2)])
-        else:
-            return str1[str1.find(str2):len(str2)]
-        '''
+        '''if str1.find(str2) == -1:
+            return ""
+            '''
+        # if str1 + str2 != str2 + str1:
+        #     return ""
+        # else :
+        #     str1 = str1[str1.find(str2):len(str2)]
+        #     if len(str1) == len(str2):
+        #         for i in (range(1,int(math.sqrt(len(str1))))):
+        #             if len(str1) % i  == 0:
+        #                 return str2[:i]
+        #             else:
+        #                 length = math.gcd(len(str1), len(str2))
+        #                 return str2[:length]
+        #     else:
+        #         length = math.gcd(len(str1), len(str2))
+        #         return str2[:length]
+
         if str1 + str2 != str2 + str1:
             return ""
 
