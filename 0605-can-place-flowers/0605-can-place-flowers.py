95ec0db69ec36e02ffa9a95c5df4fce9e4235b03
lee-JunR <68640939+lee-JunR@users.noreply.github.com>
2023-09-24 16:55:40 +0900

    Time: 147 ms (71.18%), Space: 16.6 MB (85.31%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -0,0 +1,24 @@
+#1이 나올 수 있는 조건을 세분화.
+'''
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+            if n == 0 : return True
+            for i in range(len(flowerbed)):
+                if (flowerbed[i-1] == 0 or i == 0) and flowerbed[i] == 0 and (flowerbed[i+1] == 0 or i == len(flowerbed)-1) : 
+                    flowerbed[i] = 1
+                    n -= 1
+                    if n == 0 :
+                        return True
+            return False
+'''
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if n == 0:
+            return True
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and (i == len(flowerbed)-1 or flowerbed[i+1] == 0):
+                flowerbed[i] = 1
+                n -= 1
+                if n == 0:
+                    return True
+        return False
