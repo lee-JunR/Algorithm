1a73f702231178e6ccbd87bf2f1798ba9decbcba
lee-JunR <68640939+lee-JunR@users.noreply.github.com>
2023-09-27 22:18:47 +0900

    Time: 559 ms (55.45%), Space: 28.5 MB (96.19%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
@@ -1,3 +1,19 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        res, l, r = 0, 0 ,len(nums) - 1
+
+        while l < r:
+            S = nums[l] + nums[r]
+            if S > k:
+                r -= 1
+            elif S < k:
+                l += 1
+            else:
+                res += 1
+                l += 1
+                r -= 1
+        return res
 # class Solution:
 #     def maxOperations(self, nums: List[int], k: int) -> int:
 #         count = 0
@@ -14,16 +30,16 @@
 #                 count+=1
 #         return count
 
-class Solution: 
-    def maxOperations(self, nums: List[int], k: int) -> int: 
-        count = 0 
-        num_count = collections.Counter(nums) 
-        for x in nums: 
-            if (k-x) != x and x < k and num_count[x] > 0 and num_count[k-x] > 0 : 
-                num_count[x] -= 1 
-                num_count[k-x] -= 1 
-                count+=1 
-            elif num_count[x] > 1 and x + x == k : 
-                num_count[x] -= 2 
-                count+=1 
-        return count
+# class Solution: 
+#     def maxOperations(self, nums: List[int], k: int) -> int: 
+#         count = 0 
+#         num_count = collections.Counter(nums) 
+#         for x in nums: 
+#             if (k-x) != x and x < k and num_count[x] > 0 and num_count[k-x] > 0 : 
+#                 num_count[x] -= 1 
+#                 num_count[k-x] -= 1 
+#                 count+=1 
+#             elif num_count[x] > 1 and x + x == k : 
+#                 num_count[x] -= 2 
+#                 count+=1 
+#         return count
