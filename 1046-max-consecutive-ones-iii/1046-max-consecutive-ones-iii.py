5c64e910167df62bc864fba1f0b5887d095f08ba
lee-JunR <68640939+lee-JunR@users.noreply.github.com>
2023-09-29 17:27:05 +0900

    Time: 526 ms (26.5%), Space: 17 MB (64.48%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -1,35 +1,40 @@
-# class Solution:
-#     def longestOnes(self, nums: List[int], k: int) -> int:
-
-#         count = 0
-#         s, e = 0, 1
-#         max_count = 0
-#         while e <= len(nums):
-#             if nums[s:e].count(0) <= k:
-#                 count  = (e-s)
-#                 e += 1
-#                 max_count = max(max_count, count)
-#             else :
-#                 s+=1
-#         return max_count
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        left=0
-        count_zero=0
-        max_array=0
 
-        for right in range(len(nums)):
-            if nums[right] == 0:
-                count_zero+=1
-            
-            while count_zero > k:
-                if nums[left] == 0:
-                    count_zero-=1
-                left+=1
-            
-            max_array=max(max_array,right-left+1)
+        zero_count = 0
+        start = end = max_count = 0
 
-        return max_array
+        while end < len(nums):
+            if nums[end] == 0:
+                zero_count += 1
+
+            while zero_count > k:
+                if nums[start] == 0:
+                    zero_count -= 1
+                start += 1
+
+            max_count = max(max_count, end - start + 1)
+            end += 1
+
+        return max_count
+# class Solution:
+#     def longestOnes(self, nums: List[int], k: int) -> int:
+#         left=0
+#         count_zero=0
+#         max_array=0
+
+#         for right in range(len(nums)):
+#             if nums[right] == 0:
+#                 count_zero+=1
+            
+#             while count_zero > k:
+#                 if nums[left] == 0:
+#                     count_zero-=1
+#                 left+=1
+            
+#             max_array=max(max_array,right-left+1)
+
+#         return max_array
 
 
         
