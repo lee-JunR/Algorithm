51525160ce9727ed7c2ebc35bdf0a9b368ff8d8f
lee-JunR <68640939+lee-JunR@users.noreply.github.com>
2023-10-01 21:39:21 +0900

    Time: 395 ms (88.12%), Space: 19.1 MB (16.33%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,18 +1,5 @@
 class Solution:
-    def longestSubarray(self, nums: List[int]) -> int:
-        zero_count = 0
-        start = end = max_count = 0
-
-        while end < len(nums):
-            if nums[end] == 0:
-                zero_count += 1
-
-            while zero_count > 1:
-                if nums[start] == 0:
-                    zero_count -= 1
-                start += 1
-
-            max_count = max(max_count, end - start + 1)
-            end += 1
-
-        return max_count-1
+    def longestSubarray(self, nums: list[int]) -> int:
+        streaks_of_1s = accumulate(nums, lambda a, x: (a + 1) * (x != 0), initial=0)
+        streaks_at_0 = compress(streaks_of_1s, map(not_, chain(nums, (0,))))
+        return max(starmap(add, pairwise(streaks_at_0)), default=len(nums) - 1)
