4bbc94b831a0b9cf563f371141a92fb6563e15a0
lee-JunR <68640939+lee-JunR@users.noreply.github.com>
2023-10-02 14:38:11 +0900

    Time: 41 ms (60.38%), Space: 16.3 MB (53.25%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.py
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.py
@@ -1,15 +1,8 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        sum = []
-        for i in range(len(gain)):
-            if i == 0:
-                sum.append(gain[i])
-                max_sum = sum[i]
-            else:
-                sum.append(sum[i-1]+gain[i])
-                max_sum = max(max_sum, sum[i])
-            
-        if max_sum < 0:
-            max_sum = 0
-        return max_sum
-            
+        current=0
+        highest=0
+        for x in gain:
+            current+=x
+            highest=max(current,highest)
+        return highest
