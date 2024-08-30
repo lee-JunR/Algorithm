855eadb72d1735344e26b4115ba4750fa03a9dc4
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-08-30 17:04:26 +0900

    Time: 17 ms (45.36%), Space: 47.4 MB (83.3%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.java
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -1,47 +1,58 @@
-import java.util.*;
-
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
+        // ✅ 인풋을 본인이 쓰기 편한 구조로 바꾸기 => 방향 그래프 만들기
         Map<Integer, List<int[]>> edges = new HashMap<>();
 
-        for(int[] time: times){
-            int key = time[0];
-            if(!edges.containsKey(key)){
+        for (int[] t : times) {
+            int key = t[0];
+
+            if (!edges.containsKey(key)) {
                 edges.put(key, new ArrayList<>());
             }
-            edges.get(key).add(time);
+
+            edges.get(key).add(t);
         }
+        
+        // 다익스트라 알고리즘을 호출하여 최단거리를 계산
         return dijkstra(edges, n, k);
     }
 
-    private int dijkstra(Map<Integer, List<int[]>> edges, int n, int k){
-        int[] vis = new int[n + 1];
-        Arrays.fill(vis, Integer.MAX_VALUE);
+    private int dijkstra(Map<Integer, List<int[]>> edges, int n, int k) {
+        int[] visited = new int[n + 1];
+        Arrays.fill(visited, Integer.MAX_VALUE);
+        // 큐에는 { 정점, 거리 } 형태의 배열이 들어간다.
         Queue<int[]> pq = new PriorityQueue<>((e1, e2) -> e1[1] - e2[1]);
-        pq.add(new int[] {k,0});
-        vis[k] = 0;
+        pq.add(new int[] { k, 0 });
+        visited[k] = 0;
 
-        int maxTime =0;
+        int maxTime = 0;
         int visitCount = 1;
-        while(!pq.isEmpty()){
+        while (!pq.isEmpty()) {
             int[] cur = pq.remove();
-            int u = cur[0];
-            int time = cur[1];
-            if(vis[u] < time) continue;
+            int u = cur[0], time = cur[1];
+            if (visited[u] < time)
+                continue;
             maxTime = time;
 
-            if(!edges.containsKey(u)) continue;
-            for(int[] edge : edges.get(u)){
-                int v= edge[1];
-                int w = edge[2];
+            // 연결된 모든 간선에 대해 탐색한다.
+            if (!edges.containsKey(u))
+                continue;
+            for (int[] edge : edges.get(u)) {
+                int v = edge[1], w = edge[2];
+                // 이미 더 짧은 거리로 방문한 적이 있는 경우 건너뛴다.
+                if (time + w >= visited[v])
+                    continue;
 
-                if(time + w >= vis[v]) continue;
-                if(vis[v] == Integer.MAX_VALUE) visitCount++;
-
-                vis[v] = time + w;
-                pq.add(new int[] {v, time+ w});
+                // 처음 방문했다면 visitCount++
+                if (visited[v] == Integer.MAX_VALUE)
+                    visitCount++;
+                // ✅ 다익스트라가 수행되며 각 노드까지의 최단 거리 저장
+                visited[v] = time + w;
+                pq.add(new int[] { v, time + w });
             }
         }
-        return visitCount == n ? maxTime:-1;
+        // ✅ k노드로 부터 도달할 수 없는 노드가 존재하는 경우 -1을 반환한다.
+        // ✅ 그 외의 경우 저장된 최단 거리 중 최대값을 반환한다.
+        return visitCount == n ? maxTime : -1;
     }
 }
