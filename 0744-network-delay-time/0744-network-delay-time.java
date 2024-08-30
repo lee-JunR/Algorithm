db6dc3a59f2f8278680567edca91557ba7803794
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-08-30 17:04:12 +0900

    Time: 16 ms (48.97%), Space: 47.8 MB (75%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.java
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         Map<Integer, List<int[]>> edges = new HashMap<>();
+
         for(int[] time: times){
             int key = time[0];
             if(!edges.containsKey(key)){
@@ -13,41 +14,34 @@ class Solution {
         return dijkstra(edges, n, k);
     }
 
-
-
-    int dijkstra(Map<Integer, List<int[]>> edges, int n, int k){
+    private int dijkstra(Map<Integer, List<int[]>> edges, int n, int k){
         int[] vis = new int[n + 1];
         Arrays.fill(vis, Integer.MAX_VALUE);
-
         Queue<int[]> pq = new PriorityQueue<>((e1, e2) -> e1[1] - e2[1]);
         pq.add(new int[] {k,0});
-        vis[k] =0;
+        vis[k] = 0;
 
-        int maxTime = 0;
+        int maxTime =0;
         int visitCount = 1;
-
         while(!pq.isEmpty()){
             int[] cur = pq.remove();
             int u = cur[0];
             int time = cur[1];
             if(vis[u] < time) continue;
             maxTime = time;
-            if(!edges.containsKey(u)) continue;
 
+            if(!edges.containsKey(u)) continue;
             for(int[] edge : edges.get(u)){
-                int v = edge[1];
+                int v= edge[1];
                 int w = edge[2];
 
                 if(time + w >= vis[v]) continue;
                 if(vis[v] == Integer.MAX_VALUE) visitCount++;
 
                 vis[v] = time + w;
-                pq.add(new int[]{v, vis[v]});
+                pq.add(new int[] {v, time+ w});
             }
         }
-        if(visitCount == n) return maxTime;
-        return -1;
+        return visitCount == n ? maxTime:-1;
     }
-
-    
 }
