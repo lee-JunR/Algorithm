e443f326285d0bec62446f8dc9584ee48b761583
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-09-03 00:21:20 +0900

    Time: 10 ms (85.89%), Space: 48.5 MB (56.55%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.java
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -1,73 +1,62 @@
-import java.util.*;
+class Edge implements Comparable<Edge> {
+    int node, cost;
+    public Edge(int node, int cost){
+        this.node = node;
+        this.cost = cost;
+    }
+    @Override
+    public int compareTo(Edge other) {
+        return Integer.compare(this.cost, other.cost);
+    }
+}
 
 class Solution {
-
-  public int networkDelayTime(int[][] times, int n, int k) {
-    // 그래프 초기화
-    Map<Integer, List<Edge>> edges = new HashMap<>();
-    for (int[] time : times) {
-      int key = time[0];
-      if (!edges.containsKey(key)) {
-        edges.put(key, new ArrayList<>());
-      }
-      edges.get(key).add(new Edge(time[1], time[2]));
-    }
-
-    // 다익스트라 알고리즘 실행
-    return dijkstra(edges, k, n);
-  }
-
-  private int dijkstra(Map<Integer, List<Edge>> graph, int start, int size) {
-    final int INF = Integer.MAX_VALUE;
-    int[] distance = new int[size + 1];
-    Arrays.fill(distance, INF);
-
-    // 최소 힙 구현
-    Queue<Edge> pq = new PriorityQueue<>();
-    pq.add(new Edge(start, 0)); // 시작점 노드와 거리 cost를 설정
-
-    distance[start] = 0;
-
-    while (!pq.isEmpty()) {
-      // 방문
-      Edge cur = pq.remove();
-      if (distance[cur.to] < cur.cost) {
-        continue;
-      }
-      // 다음 노드 처리
-      if (graph.containsKey(cur.to)) {
-        for (Edge next : graph.get(cur.to)) {
-          int nextCost = distance[cur.to] + next.cost;
-          if (nextCost < distance[next.to]) {
-            pq.add(new Edge(next.to, nextCost));
-            distance[next.to] = nextCost;
-          }
+    public int networkDelayTime(int[][] times, int n, int k) {
+        // 초기세팅
+            // 인접리스트 만들기
+        Map<Integer, List<Edge>> graph = new HashMap<>();
+        for (int i = 1; i <= n; i++){
+            graph.put(i, new ArrayList<>());
         }
-      }
-    }
+        for (int[] time : times) {
+            int u = time[0];
+            int v = time[1];
+            int w = time[2];
+            graph.get(u).add(new Edge(v, w));
+        }
+            // costs 배열 만들기
+        int INF = Integer.MAX_VALUE;
+        int[] costs = new int[n+1];
+        Arrays.fill(costs, INF);
 
-    int max = 0;
-    for (int i = 1; i <= size; i++) {
-      if (distance[i] == INF) return -1;
-      if (max < distance[i]) {
-        max = distance[i];
-      }
+        // 다익스트라 알고리즘 실행
+        // 시작점 예약하기
+        Queue<Edge> pq = new PriorityQueue<>();
+        pq.add(new Edge(k, 0));
+        costs[k] = 0;
+        
+        while (!pq.isEmpty()) {
+            // 방문
+            Edge cur = pq.remove();
+            if (costs[cur.node] < cur.cost) continue;
+            
+            // 예약
+            for (Edge next : graph.get(cur.node)) {
+                int nextCost = costs[cur.node] + next.cost;
+                if (nextCost < costs[next.node]) {
+                    pq.add(new Edge(next.node, nextCost));
+                    costs[next.node] = nextCost;
+                }
+            }
+        }
+            
+        //costs 배열을 순회하여 max값 찾기
+        int maxTime = 0;
+        for (int i = 1; i <= n; i++){
+            if (costs[i] == INF) return -1;
+            maxTime = Math.max(maxTime, costs[i]);
+        }
+        
+        return maxTime;
     }
-    return max;
-  }
-
-  class Edge implements Comparable<Edge> {
-    public int to;
-    public int cost;
-
-    public Edge(int to, int cost) {
-      this.to = to;
-      this.cost = cost;
-    }
-
-    @Override
-    public int compareTo(Edge o) {
-      return this.cost - o.cost; // 오름차순 정렬
-    }
-  }
 }
