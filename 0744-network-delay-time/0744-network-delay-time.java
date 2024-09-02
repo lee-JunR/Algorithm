33920fed5e4442099670da8be88f95e0017421df
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-09-03 00:44:03 +0900

    Time: 19 ms (36.89%), Space: 48.9 MB (25.13%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.java
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -2,46 +2,52 @@ import java.util.*;
 
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
+        // 그래프를 인접 리스트로 표현
         Map<Integer, List<int[]>> edges = new HashMap<>();
 
-        for(int[] time: times){
-            int key = time[0];
-            if(!edges.containsKey(key)){
-                edges.put(key, new ArrayList<>());
-            }
-            edges.get(key).add(time);
+        // 그래프 초기화
+        for (int[] time : times) {
+            edges.computeIfAbsent(time[0], key -> new ArrayList<>()).add(new int[] {time[1], time[2]});
         }
+
+        // 다익스트라 알고리즘 실행
         return dijkstra(edges, n, k);
     }
 
-    private int dijkstra(Map<Integer, List<int[]>> edges, int n, int k){
+    private int dijkstra(Map<Integer, List<int[]>> edges, int n, int k) {
         int[] vis = new int[n + 1];
         Arrays.fill(vis, Integer.MAX_VALUE);
         Queue<int[]> pq = new PriorityQueue<>((e1, e2) -> e1[1] - e2[1]);
-        pq.add(new int[] {k,0});
+        pq.add(new int[] {k, 0});
         vis[k] = 0;
 
-        int maxTime =0;
+        int maxTime = 0;
         int visitCount = 1;
-        while(!pq.isEmpty()){
+
+        while (!pq.isEmpty()) {
             int[] cur = pq.remove();
             int u = cur[0];
             int time = cur[1];
-            if(vis[u] < time) continue;
+
+            if (vis[u] < time) continue;
             maxTime = time;
 
-            if(!edges.containsKey(u)) continue;
-            for(int[] edge : edges.get(u)){
-                int v= edge[1];
-                int w = edge[2];
+            // 이 노드가 연결된 간선이 없다면 continue 없이 다음으로 넘어감
+            List<int[]> neighbors = edges.get(u);
+            if (neighbors == null) continue;
 
-                if(time + w >= vis[v]) continue;
-                if(vis[v] == Integer.MAX_VALUE) visitCount++;
+            for (int[] edge : neighbors) {
+                int v = edge[0];
+                int w = edge[1];
+
+                if (time + w >= vis[v]) continue;
+                if (vis[v] == Integer.MAX_VALUE) visitCount++;
 
                 vis[v] = time + w;
-                pq.add(new int[] {v, time+ w});
+                pq.add(new int[] {v, time + w});
             }
         }
-        return visitCount == n ? maxTime:-1;
+
+        return visitCount == n ? maxTime : -1;
     }
 }
